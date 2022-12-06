e6e8cc525cd4ab4459ebcdcced571861f3b2a4b5
Francisco_Arguedas <farguedas99@gmail.com>
2022-12-05 23:37:32 -0600

    Query Snapshot

--- firebase.js
+++ firebase.js
@@ -6,17 +6,16 @@ const app = express();
 
 app.use(express.json());
 
-app.get("/", async(req, res) =>{
-    const snapshot = await User.get();
-    const list = snapshot.docs.map((doc)=>doc.data());
-    res.send(list);
+app.get("/users", async (req, res) => {
+  const querySnapshot = await User.get();
+  const list = querySnapshot.docs.map((doc) => doc.data());
+  res.send(list);
 });
 
-app.post("/newUser", async(req, res)=>{
-
-    const data = req.body;
-    await User.add(data);
-    res.send({msg: "User added"});
-})
+app.post("/newUser", async (req, res) => {
+  const data = req.body;
+  await User.add(data);
+  res.send({ msg: "User added" });
+});
 
 app.listen(3000, () => console.log("App running on port 3000"));
